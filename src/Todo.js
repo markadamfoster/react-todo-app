1c6bf607c8c2fbd0ea6cd9baa75ba6cb75af5d47
Mark Foster <foster154@gmail.com>
2020-01-04 17:34:29 -0500

    able to update a todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { CheckSquare, Square, Trash2 } from "react-feather";
 import styled from "styled-components";
@@ -10,11 +10,25 @@ Todo.propTypes = {
 };
 
 function Todo({ todo, updateTodo, deleteTodo }) {
+  const [showInput, setShowInput] = useState(false);
+  const [value, setValue] = useState(todo.text);
+
   const handleCheckboxClick = () => {
     updateTodo({
       ...todo,
       checked: !todo.checked
     });
+  };
+
+  const handleTextUpdate = e => {
+    e.preventDefault();
+    updateTodo({ ...todo, text: value });
+    setShowInput(false);
+  };
+
+  const reset = () => {
+    setValue(todo.text);
+    setShowInput(false);
   };
 
   return (
@@ -27,7 +41,19 @@ function Todo({ todo, updateTodo, deleteTodo }) {
         )}
       </Checkbox>
 
-      <Text checked={todo.checked}>{todo.text}</Text>
+      {showInput ? (
+        <form onSubmit={e => handleTextUpdate(e)}>
+          <Input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            onBlur={reset}
+          />
+        </form>
+      ) : (
+        <Text checked={todo.checked} onClick={() => setShowInput(true)}>
+          {todo.text}
+        </Text>
+      )}
 
       <Actions>
         <DeleteButton onClick={() => deleteTodo(todo.id)}>
@@ -55,6 +81,12 @@ const Checkbox = styled.button`
   background-color: transparent;
   height: auto;
   padding: 6px 5px 2px;
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+  color: white;
 `;
 
 const Text = styled.div`
